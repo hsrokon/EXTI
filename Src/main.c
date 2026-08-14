0251310534870b9e2ee7f16d92b1367d8d3cf0eb
hsrokon <hsrokonofficial@gmail.com>
2026-08-15 00:15:10 +0600

    basic clock enabled for peripherals

--- Src/main.c
+++ Src/main.c
@@ -4,4 +4,12 @@
 
 void main(){
 //EXTI code starts here
+
+	//7.3.7
+	RCC->APB2ENR = (1<<2);//GPIOA clock enabled
+	RCC->APB2ENR = (1<<0); //Alternate Function Input/Output clock Enabled
+
+	GPIOA->CRL = ();//unfinished
+
+
 }
