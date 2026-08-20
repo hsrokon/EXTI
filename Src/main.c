47c880b92bcfeb7805e3c407953a21bb0dc6e307
hsrokon <hsrokonofficial@gmail.com>
2026-08-20 13:12:27 +0600

    configuring inpu and output pins

--- Src/main.c
+++ Src/main.c
@@ -10,7 +10,8 @@ void main(){
 				   |(1<<0); //Alternate Function Input/Output clock Enabled
 
 	GPIOA->CRL &= ~(0xFF<<0);
-	GPIOA->CRL |= d;//Unfinished
+	GPIOA->CRL |= (0x8<<0);//Configuring pin A0 as input mode pull up/down
+	GPIOA->CRL |= (0x3<<4);//Configuring pin A1 as GPO Max 50 Hz
 
 
 }
