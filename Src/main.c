9b7e44362f6246a92c0682aab2142caae692597d
hsrokon <hsrokonofficial@gmail.com>
2026-08-21 12:41:51 +0600

    configured exti0 for pa0

--- Src/main.c
+++ Src/main.c
@@ -6,12 +6,13 @@ void main(){
 //EXTI code starts here
 
 	//8.3.7
-	RCC->APB2ENR = (1<<2)//GPIOA clock enabled
+	RCC->APB2ENR |= (1<<2)//GPIOA clock enabled
 				   |(1<<0); //Alternate Function Input/Output clock Enabled
 
 	GPIOA->CRL &= ~(0xFF<<0);
 	GPIOA->CRL |= (0x8<<0);//Configuring pin A0 as input mode pull up/down
 	GPIOA->CRL |= (0x3<<4);//Configuring pin A1 as GPO Max 50 Hz
 
-
+	AFIO->EXTICR[0] &= ~(0xF<<0);//Clearing EXTI0
+	AFIO->EXTICR[0] |= (0x0<<0);//Connecting EXTI0 for PA0
 }
