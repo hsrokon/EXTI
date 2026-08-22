a681a956f93d372db9be9a9712fda1ea9fad90f0
hsrokon <hsrokonofficial@gmail.com>
2026-08-22 12:42:02 +0600

    set the ODR for PA0 to pull up resistor

--- Src/main.c
+++ Src/main.c
@@ -10,8 +10,11 @@ void main(){
 				   |(1<<0); //Alternate Function Input/Output clock Enabled
 
 	GPIOA->CRL &= ~(0xFF<<0);
-	GPIOA->CRL |= (0x8<<0);//Configuring pin A0 as input mode pull up/down
-	GPIOA->CRL |= (0x3<<4);//Configuring pin A1 as GPO Max 50 Hz
+	GPIOA->CRL |= (0x8<<0);//Configuring PA0 as input mode pull up/down
+	GPIOA->ODR |= (1<<0);//Setting PA0(as it is input) to pull up resistor.
+
+	GPIOA->CRL |= (0x3<<4);//Configuring PA1 as GPO Max 50 Hz output
+
 
 	AFIO->EXTICR[0] &= ~(0xF<<0);//Clearing EXTI0
 	AFIO->EXTICR[0] |= (0x0<<0);//Connecting EXTI0 for PA0
